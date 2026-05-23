deac6b9fbd2d2b38bb90018759a47fb1b5a0be9f
Tejas Angadi <tejasangadi456@gmail.com>
2026-05-23 20:12:28 +0530

    fix: update IAM policy for DynamoDB access with correct casing and resource reference

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,20 +40,21 @@ resource "aws_iam_role" "lambda_role" {
 resource "aws_iam_role_policy" "lambda_dynamodb" {
   name = "lambda_dynamodb_policy"
   role = aws_iam_role.lambda_role.id
+
   policy = jsonencode({
-    version = "2012-10-17"
-    statement = [{
-      effect = "Allow"
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
       Action = [
         "dynamodb:PutItem",
         "dynamodb:GetItem",
         "dynamodb:Scan",
         "dynamodb:DeleteItem"
       ]
+      Resource = aws_dynamodb_table.tasks.arn
     }]
   })
 }
-
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
