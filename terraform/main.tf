f46132c790e8c7b44f22ed94de99ecaed88aba78
Tejas Angadi <tejasangadi456@gmail.com>
2026-05-23 13:21:35 +0530

    feat: add DynamoDB table and IAM role for Lambda function in Terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,38 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
   region = "ap-south-1"
+}
+
+resource "aws_dynamodb_table" "tasks" {
+  name         = "tasks"
+  billing_mode = PAY_PER_REQUEST
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "task_api_lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = ["lambda.amazonaws.com"]
+      }
+    }]
+  })
 }
