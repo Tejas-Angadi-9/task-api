1bfea7cfc791d5757847a42c696f7082e65b6459
Tejas Angadi <tejasangadi456@gmail.com>
2026-05-23 19:54:13 +0530

    feat: add API Gateway configuration and permissions for Lambda integration

--- terraform/main.tf
+++ terraform/main.tf
@@ -73,3 +73,35 @@ resource "aws_lambda_function" "task_api" {
     }
   }
 }
+
+resource "aws_apigatewayv2_api" "task_api" {
+  name          = "task-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "task_api" {
+  api_id      = aws_apigatewayv2_api.task_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_apigatewayv2_integration" "task_api" {
+  api_id           = aws_apigatewayv2_api.task_api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.task_api.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "task_api" {
+  api_id    = aws_apigatewayv2_api.task_api.id
+  route_key = "ANY /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.task_api.id}"
+}
+
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.task_api.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.task_api.execution_arn}/*/*"
+}
+
