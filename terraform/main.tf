5bd3d22222c8271af290c1cef44a1081c2effa04
Tejas Angadi <tejasangadi456@gmail.com>
2026-05-23 13:34:34 +0530

    feat: add IAM role policy attachment for Lambda logging

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,3 +53,8 @@ resource "aws_iam_role_policy" "lambda_dynamodb" {
     }]
   })
 }
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+    role = aws_iam_role.lambda_role.name
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
