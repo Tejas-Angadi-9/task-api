ef202a90d32897df7d51a98383fb05b0ba99d202
Tejas Angadi <tejasangadi456@gmail.com>
2026-05-23 14:48:23 +0530

    feat: add AWS Lambda function configuration to Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,6 +55,21 @@ resource "aws_iam_role_policy" "lambda_dynamodb" {
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
-    role = aws_iam_role.lambda_role.name
-    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "task_api" {
+  filename      = "../lambda.zip"
+  function_name = "task-api"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "dist/lambda.handler"
+  runtime       = "nodejs20.x"
+  timeout       = 30
+
+  environment {
+    variables = {
+      TABLE_NAME = aws_dynamodb_table.tasks.name
+    }
+  }
 }
