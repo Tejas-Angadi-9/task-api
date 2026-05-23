e6a39447598d4403f92ba01d21bdd0ef01907717
Tejas Angadi <tejasangadi456@gmail.com>
2026-05-23 10:56:20 +0530

    feat: add ITask interface and implement task CRUD operations

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,14 +1,79 @@
 import { Router } from "express";
+import { ITask } from "../interfaces/task.interface";
 
 const taskRouter = Router();
 
-let tasks: any[] = [];
+// Mock Tasks DB
+let tasks: ITask[] = [];
 
+// Helper function
+const findTask = (id: string): ITask | undefined => {
+  const foundTask = tasks.find((task: ITask) => task.id === id);
+  return foundTask;
+};
+
+// Routes with logic
 taskRouter.get("/", (req, res) => {
-  res.status(200).json({
+  return res.status(200).json({
     success: true,
     tasks,
     message: "Fetched all tasks successfully",
+  });
+});
+
+taskRouter.post("/", (req, res) => {
+  const { title, description } = req.body;
+  const newTask: ITask = {
+    id: Date.now().toString(),
+    title,
+    description,
+    createdAt: new Date().toISOString(),
+  };
+
+  tasks.push(newTask);
+
+  return res.status(201).json({
+    sucess: true,
+    task: newTask,
+    message: "Created task successfully",
+  });
+});
+
+taskRouter.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const foundTask: ITask | undefined = findTask(id);
+
+  if (!foundTask) {
+    return res.status(404).json({
+      success: false,
+      message: "Task not found",
+    });
+  }
+
+  return res.status(200).json({
+    success: true,
+    task: foundTask,
+    message: "Fetched task successfully",
+  });
+});
+
+taskRouter.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const foundTask: ITask | undefined = findTask(id);
+
+  if (!foundTask) {
+    return res.status(404).json({
+      success: false,
+      message: "Task not found",
+    });
+  }
+
+  tasks = tasks.filter((task) => task.id !== id);
+  return res.status(200).json({
+    success: true,
+    task: foundTask,
+    message: "Deleted task successfully",
   });
 });
 
